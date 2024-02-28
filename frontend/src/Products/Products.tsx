a7d652bafd2d81217184d6c92c412c1eb5940b72
Daniel Marsh <Marsh.2371@osu.edu>
2024-02-28 17:37:11 -0500

    git push

--- frontend/src/Products/Products.tsx
+++ frontend/src/Products/Products.tsx
@@ -1,10 +1,13 @@
+import data from "./data";
+
 function Products() {
-    return (
-      <div>
-        <h2>Products</h2>
-      </div>
-    );
-  }
-  
+    function Products() {
+        return (
+          <div className="content">
+            <ul className="products"></ul>
+          </div>
+        );
+      }
+      
   export default Products;
-  
+    }
---
+++ frontend/src/Products/data.tsx
@@ -0,0 +1,33 @@
+export default {
+    products: [
+      {
+        name: 'Shirt',
+        imageUrl: '/images/d1.jpg',
+        price: 60,
+        brand: 'Nike',
+        rating: 4.5,
+        numberOfReviews: 10,
+      },
+      {
+        name: 'Shirt',
+        imageUrl: '/images/d1.jpg',
+        price: 60,
+        brand: 'Nike',
+        rating: 4.5,
+        numberOfReviews: 10,
+      },
+      {
+        name: 'Pants',
+        imageUrl: '/images/d2.jpg',
+        price: 45,
+        brand: 'Nike',
+        rating: 4.0,
+        numberOfReviews: 8,
+      },
+      {
+        name: 'Pants',
+        imageUrl: '/images/d3.jpg',
+        price: 25,
+        brand: 'Nike',
+        rating: 4.2,
+        numberOfReviews: 4,
