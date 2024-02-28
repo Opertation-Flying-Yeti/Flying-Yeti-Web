1dd32c2dbf627ce313cce4236a7f3a9d7bbdbb7a
Daniel Marsh <Marsh.2371@osu.edu>
2024-02-28 16:20:28 -0500

    some errors changing app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,22 +3,25 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const openMenu = () => {
+    document.querySelector(".sidebar")?.classList.add("open");
+}
+
+const closeMenu = () => {
+    document.querySelector(".sidebar")?.classList.remove("open");
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grid-container">
+            <header className="header">
+                <div className="brand">
+                    <button onClick="openMenu()">☰</button>
+                    <a href="index.html">Flying Yeti</a>
+                </div>
+                <div className="header-links">
+                    <a href="cart.html">Cart</a>
+                    <a href="signin.html">Sign In</a>
+                </div>
     </div>
   );
 }
